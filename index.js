3e786f634b8ce01eb0a02dec9e7e6a1b8b37ba0c
Aftab Alam <aftab.alam01@nagarro.com>
2023-05-10 00:38:58 +0530

    changes

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
+// Define the PORT number to use
 const PORT = 8000;
+
+// Import the express framework and create an instance of the app
 const express = require('express');
 const app = express();
-const router = require('./route')
+
+// Import the router from a separate file
+const router = require('./route');
+
+// Import the express-ejs-layouts module
 const expressLayouts = require('express-ejs-layouts');
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// Parse incoming request bodies in a middleware before the handlers
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Set up the directory for static files
 app.use(express.static('./assets'));
 
+// Set up the app to use the express-ejs-layouts module
 app.use(expressLayouts);
-// extract style and scripts from sub pages into the layout
+
+// Extract styles and scripts from sub-pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-app.use('/', router)
+// Set up the router for the root URL
+app.use('/', router);
 
-// set up the view engine
+// Set up the view engine to use ejs
 app.set('view engine', 'ejs');
+
+// Set up the directory where the view templates are located
 app.set('views', './views');
 
-app.listen(PORT, ()=>{console.log("connected to server : "+ PORT) })
+// Start the app listening on the specified PORT
+app.listen(PORT, () => {
+  console.log('connected to server: ' + PORT);
+});
